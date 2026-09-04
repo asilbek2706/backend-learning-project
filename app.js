dc376f5d946116f13c8f0f078a6ac9a62527b0db
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-04 19:57:46 +0500

    #3. Database & env

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+const postModel = require("./models/post.model");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-  // res.status(400).json({ message: "Hello World!" });
+app.get("/", async (req, res) => {
+  try {
+    const allPosts = await postModel.find();
+    res.status(200).json(allPosts);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching posts", error });
+  }
 });
 
-app.post("/post", (req, res) => {
-  const { firstName, lastName, age } = req.body;
-  const message = `Hello ${firstName} ${lastName}, you are ${age} years old!`;
-  res.send(message);
+app.post("/", async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const newPost = await postModel.create({ title, body });
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating post", error });
+  }
 });
 
 app.delete("/:id", (req, res) => {
@@ -26,7 +38,20 @@ app.put("/:id", (req, res) => {
   res.json({ message: `Update request received for ID: ${id}`, body });
 });
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Listening on - http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 8080;
+const DB_URL = process.env.DB_URL;
+
+const bootstrap = async () => {
+  try {
+    await mongoose
+      .connect(DB_URL)
+      .then(() => console.log("Connected to the database"));
+    app.listen(PORT, () => {
+      console.log(`Listening on - http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error connecting to the database:", error);
+  }
+};
+
+bootstrap();
